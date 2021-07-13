4ae365dd4ab9852c2a09f4f76a6eb4530c65a7f9
alevi1555 <alevi1555@gmail.com>
2021-07-13 22:38:44 +0300

    Created Event model

--- event_reminder/models.py
+++ event_reminder/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from event_reminder.managers import CustomUserManager
@@ -19,6 +21,34 @@ class CustomUser(AbstractUser):
 
 class Country(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+
+    REMINDER_UNITS = (
+        (60, 'minutes'),
+        (3600, 'hours'),
+        (86400, 'days'),
+        (604800, 'weeks')
+    )
+
+    name = models.TextField()
+    datetime_start = models.DateTimeField()
+    time_end = models.TimeField(null=True, blank=True, default='23:59:59')
+    remind_unit = models.IntegerField(choices=REMINDER_UNITS, null=True, blank=True)
+    number_of_remind_units = models.SmallIntegerField(default=1)
+    remind_time = models.DateTimeField(null=True, blank=True)
+    reminded = models.BooleanField(default=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user_event')
+
+    def save(self, **kwargs):
+        if self.remind_unit:
+            delta = datetime.timedelta(seconds=self.remind_unit * self.number_of_remind_units)
+            self.remind_time = self.datetime_start - delta
+        super().save(**kwargs)
 
     def __str__(self):
         return self.name
