982a3b20eac4de56cc9f979966bba70f3dbd9b69
alevi1555 <alevi1555@gmail.com>
2021-07-28 23:42:46 +0300

    Changed requirements

--- CalendarAPI/settings.py
+++ CalendarAPI/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,6 +82,15 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
         'TIME_ZONE': 'Europe/Minsk'
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': environ.get('POSTGRES_DB'),
+    #     'USER': environ.get('POSTGRES_USER'),
+    #     'PASSWORD': environ.get('POSTGRES_PASSWORD'),
+    #     'PORT': 5432,
+    #     'HOST': 'db',
+    # }
 }
 
 # Password validation
